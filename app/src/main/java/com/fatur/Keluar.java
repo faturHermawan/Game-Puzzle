8414b0fc6d138b4e140aa1a0ca8643918cb26214
faturHermawan <faturhermawan18januari1998@gmail.com>
2020-10-02 09:42:32 +0700

    MyApplication2

---
+++ app/src/main/java/com/fatur/Keluar.java
@@ -0,0 +1,11 @@
+package com.fatur;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class Keluar {
+}
